3465db9943854989a4ee0bd5940679fe8aca68b7
omar-nav <omar.navarro@g.austincc.edu>
2018-09-20 09:57:26 -0500

    ready to deploy :smile:

--- models/User.js
+++ models/User.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema
 const PLM = require('passport-local-mongoose')
 
 const userSchema = new Schema({
-  username:String,
-  email:String,
-  photoUrl:String
-},{
-  timestamps:{
-    createdAt:"created_at",
-    updatedAt:"updated_at"
-  }
-})
+  username: String,
+  email: String,
+  photoURL: String
+}, {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  })
 
-userSchema.plugin(PLM,{usernameField:'email'})
-module.exports = mongoose.model('User',userSchema)
+userSchema.plugin(PLM, { usernameField: 'email' })
+module.exports = mongoose.model('User', userSchema)
